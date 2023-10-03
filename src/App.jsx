103e8adb8b8f641f2b106d815c8e0987969a95b9
mahamrana703 <131800765+mahamrana703@users.noreply.github.com>
2023-10-03 15:09:30 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Navbar from "./components/Navbar";
+import Sliderselect from "./components/Sliderselect";
+import Tenureselect from "./components/Tenureselect";
+import Results from "./components/Results";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Sliderselect />
+      <Tenureselect />
+      <Results />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Tenureselect.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Tenureselect = () => {
+  return <div>Tenureselect</div>;
+};
+
+export default Tenureselect;
